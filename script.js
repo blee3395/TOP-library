25fd28a24e1d935e9232835ba73c30823ddee9a4
Brennan Lee <blee3395@gmail.com>
2021-11-27 21:11:40 -0500

    Working cards, rating slider, read toggle, review placement, and initial CSS styling

--- script.js
+++ script.js
@@ -6,16 +6,30 @@ let title = document.getElementById("title");
 let author = document.getElementById("author");
 let pages = document.getElementById("pages");
 let read = document.getElementById("read");
+let rating = document.getElementById("rating");
+let ratingLbl = document.querySelector('.rating');
+
+read.addEventListener('click', () => {
+    if (read.checked) {
+        ratingLbl.style.display = '';
+        rating.style.display = '';
+    } 
+    else {
+        ratingLbl.style.display = 'none';
+        rating.style.display = 'none';
+    }
+})
+
 
 document.querySelector(".submit").addEventListener('click', addBookToLibrary);
 
-
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, rating) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.rating = rating;
+    this.review = "";
     this.info = function() {
         if (this.read)
         {
@@ -36,36 +50,78 @@ function display(book) {
     let container = document.createElement('div');
     container.className = "container";
 
-    let info = document.createElement('p');
+    //Container for Title, Author, Pgs, Read status
+    let info = document.createElement('div');
     info.className = 'info';
-    info.textContent = book.info();
 
-    // let title = document.createElement('p');
-    // let author = document.createElement('p');
-    // let pages = document.createElement('p');
-    // let read = document.createElement('p');
+    //Container for Rating and Review
+    let r = document.createElement('div');
+    r.className = 'r';
+
+    //Container for Buttons
+    let btn = document.createElement('div');
+    btn.className = 'btn';
+
+    let title = document.createElement('p');
+    title.className = 'title';
+    let author = document.createElement('p');
+    author.className = 'author';
+    let pages = document.createElement('p');
+    pages.className = 'pages';
+    let read = document.createElement('p');
+    read.className = 'read';
+    let rating = document.createElement('div');
+    let review = document.createElement('p');
+    review.style.overflowY = 'scroll';
+
+    let editBtn = document.createElement('button');
     let readBtn = document.createElement('button');
+    let reviewBtn = document.createElement('button');
     let removeBtn = document.createElement('button');
 
-    // title.textContent = book.title;
-    // author.textContent = book.author;
-    // pages.textContent = book.pages;
-    // book.read ? read.textContent = 'Read' : read.textContent = 'Have not read';
+    title.textContent = book.title;
+    author.textContent = 'By ' + book.author;
+    pages.textContent = book.pages + ' pgs';
+    book.read ? read.textContent = 'Read' : read.textContent = 'Have not read';
+    if (book.read) {
+        rating = addStars(rating, book.rating);
+    } 
+    else {
+        rating.textContent = "";  
+    } 
+    book.review ? review.textContent = "Review: " +book.review : null;
+
+    editBtn.textContent = 'Edit';
+    editBtn.className = 'editBtn';
+    editBtn.addEventListener('click', toggleEdit);
+
     readBtn.textContent = 'Toggle Read';
-    readBtn.className = 'read';
+    readBtn.className = 'readBtn';
     readBtn.addEventListener('click', toggleRead);
     
+    reviewBtn.textContent = 'Review';
+    reviewBtn.className = 'reviewBtn';
+    reviewBtn.addEventListener('click', toggleReview);
+
     removeBtn.textContent = 'Remove';
-    removeBtn.className = 'remove';
+    removeBtn.className = 'removeBtn';
     removeBtn.addEventListener('click', remove);
 
-    // container.appendChild(title);
-    // container.appendChild(author);
-    // container.appendChild(pages);
-    // container.appendChild(read);
+    info.appendChild(title);
+    info.appendChild(author);
+    info.appendChild(pages);
+    info.appendChild(read);
+
+    r.appendChild(rating);
+    r.appendChild(review);
+    
+    btn.appendChild(readBtn);
+    btn.appendChild(reviewBtn);
+    btn.appendChild(removeBtn);
+   
     container.appendChild(info);
-    container.appendChild(readBtn);
-    container.appendChild(removeBtn);
+    container.appendChild(r);
+    container.appendChild(btn);
 
     return container;
 }
@@ -76,12 +132,13 @@ function reload() {
     }
     for (let i = 0; i < myLibrary.length; i++) {
         let container = display(myLibrary[i]);
+        
+        //Add index per myLibrary array to track
+        //Remove styling to make invisible
         let index = document.createElement('p');
         index.className = 'index';
         index.textContent = i+1;
-
         container.appendChild(index);
-
         index.style.display = 'none';
 
         page.appendChild(container);
@@ -89,17 +146,42 @@ function reload() {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(new Book(title.value, author.value, pages.value, read.checked));
+    myLibrary.push(new Book(title.value, author.value, pages.value, read.checked, rating.value));
 
     reload();
+}
 
-    console.log(read.checked);
-    console.log(myLibrary);
+function addStars(node, num) {
+    for (let i = 0; i < num; i++)
+    {
+        let star = document.createElement('img');
+        star.setAttribute("src", "images/star.png")
+        star.style.height = '25px';
+        star.style.weight = '25px';
+        star.setAttribute('alt','star');
+
+        node.appendChild(star);
+    }
+    node.style.gap = '4px';
+
+    return node;;
+}
+
+function toggleEdit(e) {
+//     let container = e.target.parentNode;
+//     let index = container.querySelector('.index').textContent;
+
+//     myLibrary[index-1]
+
 }
 
 function toggleRead(e) {
-    let container = e.target.parentNode;
+    let container = e.target.parentNode.parentNode;
     let index = container.querySelector('.index').textContent;
+    
+    console.log(container);
+    console.log(index);
+
     let toggle = container.querySelector('#read');
 
     if (myLibrary[index-1].read) {
@@ -112,8 +194,19 @@ function toggleRead(e) {
     reload();
 }
 
+function toggleReview(e) {
+    let container = e.target.parentNode.parentNode;
+    let index = container.querySelector('.index').textContent;
+
+console.log(container.querySelector('.index'));
+
+    myLibrary[index-1].review = prompt("Leave your review here.", "What I learned in boating school is...");
+
+    reload();
+}
+
 function remove(e) {
-    let container = e.target.parentNode;
+    let container = e.target.parentNode.parentNode;
     let index = container.querySelector('.index').textContent;
 
     myLibrary.splice(index-1, 1);
